56f4a369d8204f18afc23cf501c750fe0fecc613
Banee Ishaque K <Baneeishaque@gmail.com>
2018-09-20 01:30:30 +0530

    All

--- src/Combinations.java
+++ src/Combinations.java
@@ -4,9 +4,16 @@ public class Combinations {
     public static void main(String[] args) {
 
         // Create an alphabet to work with
-        char[] alphabet = new char[]{'a', 'b'};
+        char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
         // Find all possible combinations of this alphabet in the string size of 3
         StringExcersise.possibleStrings(3, alphabet, "");
+        StringExcersise.possibleStrings(4, alphabet, "");
+        StringExcersise.possibleStrings(5, alphabet, "");
+        StringExcersise.possibleStrings(6, alphabet, "");
+        StringExcersise.possibleStrings(7, alphabet, "");
+        StringExcersise.possibleStrings(8, alphabet, "");
+        StringExcersise.possibleStrings(9, alphabet, "");
+        StringExcersise.possibleStrings(10, alphabet, "");
     }
 
 }
